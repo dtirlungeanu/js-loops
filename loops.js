0dc13f2d2cb6ceb7b0ffc36a37dacdf954938e00
Dragos Tirlungeanu <Dragos@dragoss-mbp.home>
2018-02-11 17:50:01 +0000

    this is the first commit for loops

---
+++ loops.js
@@ -0,0 +1,39 @@
+// LOOPS ITTERATE THROUH AN ARRAY OR A RESULT FROM A DATABASE ETC..
+// for loop
+// while loop
+// do while loop
+// for in
+
+// var x = 0 --> INITIALISATION
+// while (CONDITION)
+// INCREASE	(increment)
+//do while loop:
+
+//for in loop:
+//var MyArray = [6, 2, ,8 ,9 ,15];
+//for(var x in MyArray){
+// 	console.log(x);
+// } -- >> this gives the lsit
+
+// for (INITIALASATION ; CONDITION ; INCREASE(increment by..[++ is 1]))
+for (var i = 0; i < 10; i++) {
+	console.log(i);
+}
+
+
+var x = 0;
+while (x<10){
+	x++;
+}
+
+
+var x = 2;
+do {
+	console.log("My string");
+} while(x<6);
+
+
+var MyArray = [6, 2, ,8 ,9 ,15];
+for(var x in MyArray){
+	console.log(MyArray[x]);
+}
